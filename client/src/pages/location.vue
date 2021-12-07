32f4145a29f4f7322d62845a16019170c0ba1e03
aleem-ahmed <w3st.io2021@gmail.com>
2021-12-07 11:52:34 -0500

    Improved

--- client/src/pages/location.vue
+++ client/src/pages/location.vue
@@ -14,29 +14,30 @@
 					>
 						<BCard
 							bg-variant="white"
+							border-variant="secondary"
 							no-body
 							class="mb-3"
 						>
-							<BCardHeader>
-								<h5 v-if="a.type == 'energy-station'" class="m-0">
+							<BCardHeader class="text-primary border-secondary bg-transparent">
+								<h4 v-if="a.type == 'energy-station'" class="m-0">
 									{{ a.name }} Energy Station
-								</h5>
+								</h4>
 
-								<h5 v-if="a.type == 'store'" class="m-0">
+								<h4 v-if="a.type == 'store'" class="m-0">
 									{{ a.name }} Store
-								</h5>
+								</h4>
 
-								<h5 v-if="a.type == 'car-wash'" class="m-0">
+								<h4 v-if="a.type == 'car-wash'" class="m-0">
 									{{ a.name }} Car Wash 
-								</h5>
+								</h4>
 
-								<h5 v-if="a.type == 'service-shop'" class="m-0">
+								<h4 v-if="a.type == 'service-shop'" class="m-0">
 									{{ a.name }} Service Shop
-								</h5>
+								</h4>
 
-								<h5 v-if="a.type == 'amazon-locker'" class="m-0">
+								<h4 v-if="a.type == 'amazon-locker'" class="m-0">
 									{{ a.name }} Amazon Locker
-								</h5>
+								</h4>
 							</BCardHeader>
 
 							<BCardBody>
@@ -53,7 +54,12 @@
 								</BRow>
 
 								<!-- Products -->
-								<BRow>
+								<BRow v-if="a.products.length > 0">
+									<BCol cols="12">
+										<h6 class="text-secondary">Sold Here</h6>
+										<hr>
+									</BCol>
+
 									<BCol
 										v-for="(p, ii) in a.products"
 										:key="ii"
@@ -64,15 +70,48 @@
 									</BCol>
 								</BRow>
 
-								<!-- Services -->
-								<BRow>
+								<!-- Delivery Methods -->
+								<BRow v-if="a.deliveryMethods.length > 0">
+									<BCol cols="12">
+										<h6 class="text-secondary">Accepted Delivery Methods</h6>
+										<hr>
+									</BCol>
+
 									<BCol
 										v-for="(d, ii) in a.deliveryMethods"
 										:key="ii"
 										cols="6" sm="3"
-										class="m-0 p-0 text-center"
+										class="text-center"
 									>
-										{{ d }}
+										<img
+											v-if="d == 'Uber'"
+											:src="uberLogo"
+											:alt="d"
+											class="w-100 px-2"
+										>
+
+										<img
+											v-else-if="d == 'DoorDash'"
+											:src="doordashLogo"
+											:alt="d"
+											class="w-100 px-2"
+										>
+
+										<img
+											v-else-if="d == 'Grubhub'"
+											:src="grubhubLogo"
+											:alt="d"
+											class="w-100 px-2"
+										>
+
+										<img
+											v-else-if="d == 'Vroom'"
+											:src="vroomLogo"
+											:alt="d"
+											class="w-100 px-2"
+										>
+
+										<h6 v-else class="m-0">{{ d }}</h6>
 									</BCol>
 								</BRow>
 							</BCardBody>
@@ -82,7 +121,6 @@
 			</BCol>
 
 			<BCol cols="12" md="3">
-					
 				<BCard bg-variant="primary" text-variant="light" no-body>
 					<!-- Address -->
 					<BCardHeader class="border-0">
@@ -160,14 +198,16 @@
 			return {
 				locations: locations,
 				location: {},
+				uberLogo: require('../assets/images/uber-logo.png'),
+				doordashLogo: require('../assets/images/doordash-logo.png'),
+				grubhubLogo: require('../assets/images/grubhub-logo.jpg'),
+				vroomLogo: require('../assets/images/vroom-logo.png'),
 			}
 		},
 
 		created() {
 			this.locations.forEach(l => {
-				console.log(l);
 				if (l.id == parseInt(this.$route.params.location_id)) {
-					console.log('RAN');
 					this.location = l
 				}	
 			})

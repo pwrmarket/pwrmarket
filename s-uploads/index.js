5d09a753e463d1dc612a12d1e55d5a9c25799262
aleem-ahmed <w3st.io2021@gmail.com>
2021-12-14 00:12:18 -0500

    s

--- s-uploads/index.js
+++ s-uploads/index.js
@@ -1,4 +1,1 @@
-console.log(`
-	in order for multer to work on heroku you must have at least 1 file in the folder
-	or else heroku deletes the the empty folder
-`)
+// in order for multer to work on heroku you must have at least 1 file in the folder or else heroku deletes the the empty folder

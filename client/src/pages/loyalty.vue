58fca30cbb0a07783e8da4fcea26acae3be113ed
aleem-ahmed <w3st.io2021@gmail.com>
2021-12-15 11:12:42 -0500

    Improved

--- client/src/pages/loyalty.vue
+++ client/src/pages/loyalty.vue
@@ -9,32 +9,56 @@
 		<BContainer class="py-5">
 			<BRow>
 				<BCol cols="12" md="7">
-					<BListGroup class="shadow" data-aos="fade">
-						<BListGroupItem variant="light">
-							<h4 class="m-0 text-center text-primary">How It Works</h4> 
-						</BListGroupItem>
+					<BRow>
+						<BCol cols="12">
+							<h1 class="mb-5 text-center text-gradient">How It Works</h1>
+						</BCol>
 
-						<BListGroupItem variant="light" class="text-secondary">
-							– Download our app
-						</BListGroupItem>
+						<BCol cols="12" sm="6" lg="4">
+							<div class="text-center">
+								<SmartphoneIcon size="5x" class="mb-3 text-secondary" />
+								<h5 class="mb-3 text-primary">
+									1. Download our app
+								</h5>
+							</div>
+						</BCol>
 
-						<BListGroupItem variant="light" class="text-secondary">
-							– Sign up for your free Power Market membership
-						</BListGroupItem>
+						<BCol cols="12" sm="6" lg="4">
+							<div class="text-center">
+								<ClipboardIcon size="5x" class="mb-3 text-secondary" />
+								<h5 class="mb-3 text-primary">
+									2. Sign up for your free Power Market membership
+								</h5>
+							</div>
+						</BCol>
 
-						<BListGroupItem variant="light" class="text-secondary">
-							– Find a participating location or select an offer that you like
-						</BListGroupItem>
+						<BCol cols="12" sm="6" lg="4">
+							<div class="text-center">
+								<MapPinIcon size="5x" class="mb-3 text-secondary" />
+								<h5 class="mb-3 text-primary">
+									3. Find a participating location or select an offer that you like
+								</h5>
+							</div>
+						</BCol>
 
-						<BListGroupItem variant="light" class="text-secondary">
-							– Scan your barcode before you pay inside to earn and redeem rewards
-						</BListGroupItem>
+						<BCol cols="12" sm="6" lg="4">
+							<div class="text-center">
+								<DollarSignIcon size="5x" class="mb-3 text-secondary" />
+								<h5 class="mb-3 text-primary">
+									4. Scan your barcode before you pay inside to earn and redeem rewards
+								</h5>
+							</div>
+						</BCol>
 
-						<BListGroupItem variant="light" class="text-secondary">
-							– Just getting gas? Enter your Alt ID at the pump to apply your reward balance and pay less for fuel.
-						</BListGroupItem>
-
-					</BListGroup>
+						<BCol cols="12" sm="6" lg="4">
+							<div class="text-center">
+								<BatteryChargingIcon size="5x" class="mb-3 text-secondary" />
+								<h5 class="mb-3 text-primary">
+									5. Just getting gas? Enter your Alt ID at the pump to apply your reward balance and pay less for fuel
+								</h5>
+							</div>
+						</BCol>
+					</BRow>
 				</BCol>
 
 				<BCol cols="12" md="5">
@@ -46,6 +70,8 @@
 </template>
 
 <script>
+	import { BatteryChargingIcon, ClipboardIcon, DollarSignIcon, MapPinIcon, SmartphoneIcon } from 'vue-feather-icons'
+
 	import defaultData from '../defaults/companyInfo'
 
 	export default {
@@ -54,6 +80,14 @@
 				defaultData: defaultData,
 				image: require('../assets/images/shop.jpg')
 			}
+		},
+
+		components: {
+			BatteryChargingIcon,
+			ClipboardIcon,
+			DollarSignIcon,
+			MapPinIcon,
+			SmartphoneIcon,
 		},
 	}
 </script>
